9e581e0840aae8e82d5c1c55f735d39021667a5c
SReject <sreject@comcast.net>
2016-10-26 14:56:27 -0400

    Converted linebreaks to unix

--- bot/main.js
+++ bot/main.js
@@ -1,5 +1,6 @@
 var validate  = require('../common/validate/validate.js'),
     PluginMgr = require('../common/plugin/manager.js');
 
-module.exports = function (config) {
-}
+module.exports = {
+    init: function () { }
+};
